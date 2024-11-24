187ca96f2e657c537a34bc2b67c6e9369c4f8543
Feyyaz Can KÖSE <kesemenere123@gmail.com>
2024-11-25 00:00:49 +0300

    Last Commit

---
+++ Faly.PresentationLayer/Models/CartItemViewModel.cs
@@ -0,0 +1,8 @@
+namespace Faly.Models;
+
+public class CartItemViewModel
+{
+    public int CourseId { get; set; }
+    public string CourseName { get; set; }
+    public decimal Price { get; set; }
+}
